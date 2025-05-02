c6796fe4de46988d6f345675a3b9807bef58801f
KarenLore <karenlorenacriscaceres@gmail.com>
2025-05-02 17:02:24 -0500

    feat: :tada: Initial Project
    Initial Project

---
+++ electro-app/src/main/java/com/electroapp/electro_app/domain/entities/City.java
@@ -0,0 +1,36 @@
+package com.electroapp.electro_app.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "cities")
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Embedded
+    Audit audit = new Audit();
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    @JsonBackReference
+    Region regions;
+}
